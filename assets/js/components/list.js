00cfdce42d34b893f0f142ce469b22f0e12e5a07
dwiwicaksono90 <dwiw1909@gmail.com>
2018-10-12 13:52:19 +0700

    edit list

--- assets/js/components/list.js
+++ assets/js/components/list.js
@@ -1,5 +1,5 @@
 Vue.component('list-bounty', {
-    data: function(){
+    data: function () {
         return {
             allList: []
         }
@@ -8,22 +8,21 @@ Vue.component('list-bounty', {
     methods: {
 
     },
-    created(){
+    created() {
         axios({
-            method: 'GET',
-            url: 'http://localhost:3000/bounty'
-        })
-        .then((result) => {
-            console.log(result.data.data);
-            if(result.data){
-                this.allList = result.data.data
-            } else {
-                console.log('no bounty data');
-            }
-        }).catch((err) => {
-            console.log(err);
-        });
-       },
+                method: 'GET',
+                url: 'http://localhost:3000/bounty'
+            })
+            .then((result) => {
+                if (result.data) {
+                    this.allList = result.data.data
+                } else {
+                    console.log('no bounty data');
+                }
+            }).catch((err) => {
+                console.log(err);
+            });
+    },
     template: `
     <div class="container-fluid">
         <div class="row">
@@ -32,21 +31,21 @@ Vue.component('list-bounty', {
                 alt="Card image cap" style="height: 300px; width: 300px; margin: 0 auto">
             <div class="card-body">
                 <h3 class="card-title">{{list.name}}</h3>
-                    <h5 class="card-title">{{list.gender}}</h5>
-                <h5 class="card-title">{{list.age}}</h5>
+                    <h5 class="card-title">Gender: {{list.gender}}</h5>
+                <h5 class="card-title">Age: {{list.age}}</h5>
                 <h5 class="card-title">Hair Color: {{list.hairColor}}</h5>
             </div>
             <div class="card-body">
                 <div class="row">
                     <div class="col-sm-4">
-                    
+                        <div class="fb-share-button card-link" :data-href="list.avatar" data-layout="button_count"></div>
                     </div>
-                <div class="col-sm-4">
-                    <div class="fb-share-button card-link" data-href="https://storage.googleapis.com/bountyhunter/1539269853811adult-beard-boy-220453.jpg"
-                        data-layout="button_count"></div>
-                </div>
-                <div class="col-sm-4"><a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Help us to find someone https://storage.googleapis.com/bountyhunter/1539269853811adult-beard-boy-220453.jpg"
-                        data-size="large">Tweet</a></div>
+                    <div class="col-sm-4">
+                        <a class="twitter-share-button" :href="'https://twitter.com/intent/tweet?text=Help us to find someone ' + list.avatar "  data-size="large">Tweet</a>
+                    </div>
+                    <div class="col-sm-4">
+                        <button type="button" class="btn btn-info">Get Image</button>
+                    </div>
                 </div>
             </div>
         </div>
